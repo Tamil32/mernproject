0c2eff07e1a6b693efca287d016be00d0796f8f0
Tamil32 <tamiselvan1432@gmail.com>
2024-12-15 20:42:56 +0530

    fix : create new file

--- style.js
+++ style.js
@@ -1,1 +1,3 @@
 console .log ( "Hello World" );
+const store = [apple, banana, cherry, date, elderberry, fig, grape, honeydew, kiwi, lemon, mango, nectarine, orange, papaya, quince, raspberry, strawberry, tangerine, ugli, watermelon];
+console .log (store);
